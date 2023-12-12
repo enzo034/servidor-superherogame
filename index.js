ce7c8b4881d0087dbaa86d0e1fb09406ca09854d
Enzo Petela <enzo03453@gmail.com>
2023-12-12 20:16:47 -0300

    .

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ app.get("/", (req, res) => {
           <title>NodeJs y Express en Vercel</title>
         </head>
         <body>
-          <h1>Soy un proyecto Back end en vercel</h1>
+          <h1>Servidor funcionando!</h1>
         </body>
       </html>
     `;
--- src/app.js
+++ src/app.js
@@ -1,11 +1,15 @@
 import express from 'express';
 
+//CORS
+import cors from 'cors';
+
 //rutas
 import authRoutes from './routes/Auth.routes.js';
 import peleaRoutes from './routes/Pelea.routes.js'
 
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 
 app.use('/api/auth', authRoutes);
