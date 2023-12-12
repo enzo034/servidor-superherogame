2246fb52766a31381d768e52f7bfbd9c6a9deaa5
Enzo Petela <enzo03453@gmail.com>
2023-12-12 15:11:22 -0300

    fix

--- src/config.js
+++ src/config.js
@@ -2,7 +2,7 @@ import dotenv from 'dotenv'
 
 dotenv.config();
 
-export const PORT = process.env.PORT;
+export const PORT = process.env.PORT || 3000;
 
 export const DB_URI = process.env.DB_URI;
 
--- src/index.js
+++ src/index.js
@@ -5,6 +5,20 @@ import { connectDB } from './db.js';
 
 connectDB();
 
+app.get("/", (req, res) => {
+    const htmlResponse = `
+      <html>
+        <head>
+          <title>NodeJs y Express en Vercel</title>
+        </head>
+        <body>
+          <h1>Soy un proyecto Back end en vercel</h1>
+        </body>
+      </html>
+    `;
+    res.send(htmlResponse);
+  });
+
 app.listen(PORT);
 
 console.log("Servidor escuchando en el puerto", PORT);
