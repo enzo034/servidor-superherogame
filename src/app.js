ee6dcc7f7ddd538b0fae251bb8e9250f579e160e
Enzo Petela <enzo03453@gmail.com>
2024-01-17 10:21:35 -0300

    fixxxxx

--- src/app.js
+++ src/app.js
@@ -1,28 +1,18 @@
 import express from 'express';
+
+//CORS / middlewares
 import cors from 'cors';
+
 import helmet from 'helmet';
 
+//rutas
 import authRoutes from './routes/Auth.routes.js';
 import peleaRoutes from './routes/Pelea.routes.js';
 import userRoutes from './routes/User.routes.js';
 
 const app = express();
 
-const allowedOrigins = ['http://localhost:4200'];
-
-app.use(cors({
-  origin: function (origin, callback) {
-    // Permitir peticiones desde el mismo servidor
-    if (!origin || allowedOrigins.includes(origin)) {
-      callback(null, true);
-    } else {
-      callback(new Error('Not allowed by CORS'));
-    }
-  },
-  methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
-  credentials: true
-}));
-
+app.use(cors());
 app.use(helmet());
 app.use(express.json());
 
