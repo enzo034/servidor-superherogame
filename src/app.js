c1549feaf51b00419361b3c4d95f83f21cf8890d
Enzo Petela <enzo03453@gmail.com>
2024-01-27 22:43:28 -0300

    feat (Borrado de tokens): función hecha para borrar cada 24hrs los tokens de recuperación creados antes de la fecha actual.

--- src/app.js
+++ src/app.js
@@ -2,13 +2,16 @@ import express from 'express';
 
 //CORS / middlewares
 import cors from 'cors';
-
 import helmet from 'helmet';
 
 //rutas
 import authRoutes from './routes/Auth.routes.js';
 import peleaRoutes from './routes/Pelea.routes.js';
 import userRoutes from './routes/User.routes.js';
+
+//Borrar tokens
+import deleteOldRecoveryTokens from './utils/deleteOldRecoveryTokens.js';
+deleteOldRecoveryTokens();
 
 const app = express();
 
---
+++ src/utils/deleteOldRecoveryTokens.js
@@ -0,0 +1,17 @@
+// deleteOldRecoveryTokens.js
+
+import RecoveryToken from '../models/Token.model.js';
+
+// Define la lógica para eliminar tokens vencidos
+const deleteOldRecoveryTokens = () => {
+    setInterval(async () => {
+        try {
+            await RecoveryToken.deleteMany({ expiry: { $lt: new Date() } });
+            console.log('Tokens vencidos eliminados.');
+        } catch (error) {
+            console.error('Error al eliminar tokens vencidos:', error);
+        }
+    }, 24 * 60 * 60 * 1000); // Ejecutar cada 24 horas.
+};
+
+export default deleteOldRecoveryTokens;
