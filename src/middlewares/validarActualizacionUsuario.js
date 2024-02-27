f83e24e0c0c5d7045e787871cc31126bff4ccf55
Enzo Petela <enzo03453@gmail.com>
2024-02-27 19:03:30 -0300

    fix (validate data)

--- src/middlewares/validarActualizacionUsuario.js
+++ src/middlewares/validarActualizacionUsuario.js
@@ -3,13 +3,14 @@ import Joi from 'joi'
 const schemaActualizacionUsuario = Joi.object({
     nombre: Joi.string().pattern(/^[a-zA-Z]+$/).min(4).max(15),
     apellido: Joi.string().pattern(/^[a-zA-Z]+$/).min(4).max(15),
-    email: Joi.string().email().min(5)
+    email: Joi.string().email().min(5),
+    confirmado: Joi.boolean()
 }).min(1); // Al menos un campo debe estar presente en la solicitud
 
 export const validarActualizacionUsuario = async (req, res, next) => {
     const { nombre, apellido, email } = req.body;
 
-    const validatorResult = schemaActualizacionUsuario.validate({ nombre, apellido, email }, { abortEarly: false });
+    const validatorResult = schemaActualizacionUsuario.validate({ nombre, apellido, email, confirmado }, { abortEarly: false });
 
     if (validatorResult.error) {
         return res.status(400).json({
